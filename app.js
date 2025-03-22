01a86a1f5ae4e6bb308af1e727d4e989f9b4d112
Riz <55377396+Riz-0@users.noreply.github.com>
2025-03-22 17:54:31 +0800

    Add color on hover function

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ function createGrid(size) {
     for (let col = 0; col < size; col++) {
       const cell = document.createElement("div");
       cell.style.cssText = "width: 20px; height: 20px; border: 1px solid grey";
+      cell.addEventListener("mouseover", (e) => color(e.target));
       grid.appendChild(cell);
     }
   }
+}
+
+function color(cell) {
+  // Changes the color of a single cell
+  cell.style.backgroundColor = "#595959";
 }
 
 createGrid(16);
