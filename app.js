a3249578e9988cf620f9e4c7dda80c06ee767dd4
Riz <55377396+Riz-0@users.noreply.github.com>
2025-03-23 15:53:01 +0800

    Refactor createGrid() to maintain size when changing the number of cells

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function createGrid(size) {
   // Create a grid that is size^2
   const grid = document.querySelector("#grid");
-  grid.style.maxWidth = `${size * 20}px`;
+  const width = 600 / size;
   for (let row = 0; row < size; row++) {
     for (let col = 0; col < size; col++) {
       const cell = document.createElement("div");
-      cell.style.cssText = "width: 20px; height: 20px; border: 1px solid grey";
+      cell.style.cssText = `width: ${width}px; height: ${width}px; border: 1px solid grey`;
       cell.addEventListener("mouseover", (e) => color(e.target));
       grid.appendChild(cell);
     }
@@ -17,4 +17,4 @@ function color(cell) {
   cell.style.backgroundColor = "#595959";
 }
 
-createGrid(16);
+createGrid(100);
